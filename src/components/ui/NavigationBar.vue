bef644ec9770fb46bdd691ffe57c7acfbc331db0
Kseniya Harpushkina <KHarpushkina@gomel.iba.by>
2020-12-17 22:42:21 +0300

    Organize folder structure. Add main page and header component

---
+++ src/components/ui/NavigationBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Main Page</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link1</b-nav-item>
+          <b-nav-item href="#">Link2</b-nav-item>
+          <b-nav-item href="#">Link3</b-nav-item>
+          <b-nav-item href="#">Link4</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <b-input-group size="md" class="mb-2">
+              <b-form-input
+                placeholder="Search articles"
+              ></b-form-input>
+              <b-input-group-append is-text variant="info">
+                <Search></Search>
+              </b-input-group-append>
+            </b-input-group>
+          </b-nav-item>
+
+          <b-nav-item>
+            <b-button variant="outline-light">Sign In</b-button>
+          </b-nav-item>
+          <b-nav-item>
+            <b-button variant="light">Sign Up</b-button>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticlesList",
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/ArticllesMainPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="articles-list-section">
+    <div class="container">
+      <div
+        v-for="article in articles"
+        :key="`${article.name}`"
+        :value="`${article.name}`"
+        class="row card"
+      >
+        <div class="col">
+          <article-preview :article="article"></article-preview>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const DATA = require("../../../server/database.json");
+import ArticlePreview from "../ArticlePreview/ArticlePreview.vue";
+
+export default {
+  name: "ArticlesList",
+  components: {
+    ArticlePreview,
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+
+  methods: {
+    cutArticleContent(content) {
+      let result = content.substring(0, 1000);
+      result = result.substring(0, result.lastIndexOf(" "));
+      return result + "...";
+    },
+  },
+
+  mounted() {
+    this.articles = DATA.articles;
+  },
+};
+</script>
+
+<style scoped>
+.articles-list-section {
+  margin: 3rem;
+  padding: 1rem;
+}
+
+.row {
+  margin-bottom: 10px;
+  padding: 1rem;
+}
+</style>
