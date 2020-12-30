d339c2b695f4b0e19631c7534a2a16d5531f7a8e
Kseniya Harpushkina <KHarpushkina@gomel.iba.by>
2020-12-30 12:23:35 +0300

    Setup babel for await/async

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
     presets: ["@vue/cli-plugin-babel/preset", "@babel/preset-env"],
+    plugins: [
+        [
+            "@babel/plugin-transform-async-to-generator",
+            {
+                module: "bluebird",
+                method: "coroutine",
+            },
+        ],
+    ],
     ignore: ["node_modules"],
 };
--- src/client/store/requests.js
+++ src/client/store/requests.js
@@ -5,26 +5,12 @@ const serverURL = "http://localhost:3000";
 
 export default {
     getArticles() {
-        return axios
-            .get(serverURL + "/articles")
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        return axios.get(serverURL + "/articles");
     },
 
     addArticle(article) {
-        return axios
-            .post(serverURL + "/create-article", {
-                article
-            })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        return axios.post(serverURL + "/create-article", {
+            article,
+        });
     },
 };
